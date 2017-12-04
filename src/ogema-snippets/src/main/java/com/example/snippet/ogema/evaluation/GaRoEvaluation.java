7a9a9530b9d2be78f21d8c0246023100ca719ecf
dnestle <dnestle@10.45.20.163>
2017-12-04 23:00:06 +0100

    added OnlineNonBlockingIterator example

--- src/ogema-snippets/src/main/java/com/example/snippet/ogema/evaluation/GaRoEvaluation.java
+++ src/ogema-snippets/src/main/java/com/example/snippet/ogema/evaluation/GaRoEvaluation.java
@@ -6,8 +6,10 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import org.ogema.core.application.ApplicationManager;
 import org.ogema.core.channelmanager.measurements.SampledValue;
 import org.ogema.core.model.schedule.Schedule;
+import org.ogema.core.model.simple.FloatResource;
 import org.ogema.core.timeseries.ReadOnlyTimeSeries;
 
 import de.iwes.timeseries.eval.api.EvaluationInput;
@@ -21,7 +23,9 @@ import de.iwes.timeseries.eval.api.helper.EvalHelperExtended;
 import de.iwes.timeseries.eval.base.provider.utils.EvaluationInputImpl;
 import de.iwes.timeseries.eval.base.provider.utils.EvaluationUtils;
 import de.iwes.timeseries.eval.base.provider.utils.TimeSeriesDataImpl;
+import de.iwes.timeseries.eval.base.provider.utils.TimeSeriesDataOnlineImpl;
 import de.iwes.timeseries.eval.garo.api.helper.GaRoEvalHelper;
+import de.iwes.timeseries.eval.onlineIterator.OnlineNonBlockingIterator;
 import de.iwes.timeseries.winopen.provider.WinHeatEvalProvider;
 
 /** Example how to start GaRo evaluation
@@ -98,4 +102,73 @@ public class GaRoEvaluation {
 		Map<String, String> evalResults = EvalHelperExtended.getResults(instance);
 		return evalResults;
 	}
+	
+	public static OnlineNonBlockingIterator runSampleEvaluationOnline(FloatResource temperatureMeasurement,
+			FloatResource windowOpenLogDataWindow1, FloatResource windowOpenLogDataWindow2,
+			FloatResource valvePosition, String label, String description,
+			WinHeatEvalProvider evalProvider,
+			Collection<ConfigurationInstance> configurations,
+			Schedule resultSchedule, ApplicationManager appMan) {
+		//Requested input 1 of EvaluProvider: temperature data
+		final List<TimeSeriesData> timeSeriesDataTemperature = new ArrayList<>();
+		TimeSeriesDataOnlineImpl dataImpl = new TimeSeriesDataOnlineImpl(temperatureMeasurement, label+"_Room-Temperature", 
+				description, null);
+		timeSeriesDataTemperature.add(dataImpl);
+		
+		//Requested input 2 of EvaluProvider: window opening measurement data
+		final List<TimeSeriesData> timeSeriesDataWindows = new ArrayList<>();
+		dataImpl = new TimeSeriesDataOnlineImpl(windowOpenLogDataWindow1, label+"_Window1", 
+				description, null);
+		timeSeriesDataTemperature.add(dataImpl);
+		if(windowOpenLogDataWindow2 != null) {
+			dataImpl = new TimeSeriesDataOnlineImpl(windowOpenLogDataWindow2, label+"_Window2", 
+					description, null);
+			timeSeriesDataWindows.add(dataImpl);
+		}
+		
+		//Requested input 3 of EvaluProvider: valve position data
+		final List<TimeSeriesData> timeSeriesDataValve = new ArrayList<>();
+		dataImpl = new TimeSeriesDataOnlineImpl(valvePosition, label+"_Valve", 
+				description, null);
+		timeSeriesDataValve.add(dataImpl);
+
+		/**Build input data set to evaluation*/
+		final List<EvaluationInput> inputs = Arrays.<EvaluationInput> asList(new EvaluationInput[]{
+				new EvaluationInputImpl(timeSeriesDataTemperature),
+				new EvaluationInputImpl(timeSeriesDataWindows),
+				new EvaluationInputImpl(timeSeriesDataValve)});
+		
+		/**Change this if not all results offered by the provider shall be calculated*/
+		List<ResultType> requestedResults = evalProvider.resultTypes();
+		
+		/**Note that most evaluations do not support intermediateListeners. See 
+		 * git\fhg-alliance-internal\src\widgets\timeseries-tools\timeseries-heating-analysis-multi\src\main\java\de\iwes\timeseries\provider\outsideTemperature
+		 * for an example that supports this
+		 */
+		ResultListener intermediateListener = null;
+		if(resultSchedule != null) {
+			intermediateListener = new ResultListener() {
+
+				@Override
+				public void resultAvailable(ResultType type, SampledValue value) {
+					resultSchedule.addValue(value.getTimestamp(), value.getValue());
+				}
+				
+			};
+		}
+		/**Finally start evaluation*/
+		OnlineNonBlockingIterator onlineNBI = EvaluationUtils.startEvaluationOnline(evalProvider, inputs, requestedResults , configurations,
+				intermediateListener, appMan);
+		EvaluationInstance instance = onlineNBI.getEvaluationInstance();
+				
+		/**Get results*/
+		final Map<ResultType, EvaluationResult> results = instance.getResults();
+		
+		onlineNBI.start();
+		
+		//GaRoEvalHelper.printAllResults("Room_"+label, results, EvaluationUtils.getStartAndEndTime(configurationsAll, inputs, false));
+		//Map<String, String> evalResults = EvalHelperExtended.getResults(instance);
+		return onlineNBI;
+	}
+
 }
