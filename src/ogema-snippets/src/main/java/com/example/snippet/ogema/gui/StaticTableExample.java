83870dd3ac4314aaa69195a96105c6c0018514c1
dnestle <dnestle@10.45.20.163>
2018-03-15 19:11:01 +0100

    minor fixes

--- src/ogema-snippets/src/main/java/com/example/snippet/ogema/gui/StaticTableExample.java
+++ src/ogema-snippets/src/main/java/com/example/snippet/ogema/gui/StaticTableExample.java
@@ -6,6 +6,7 @@ import de.iwes.widgets.api.widgets.html.StaticTable;
 import de.iwes.widgets.html.form.button.Button;
 import de.iwes.widgets.html.form.textfield.TextField;
 import de.iwes.widgets.html.html5.Flexbox;
+import de.iwes.widgets.html.html5.flexbox.FlexDirection;
 import de.iwes.widgets.html.html5.flexbox.FlexWrap;
 import de.iwes.widgets.html.html5.flexbox.JustifyContent;
 
@@ -19,11 +20,20 @@ public class StaticTableExample {
 	        table2.setContent(0, 0, editNewProgramName);
 	        table2.setContent(0, 1, buttonSaveAsNewProgram);
 	        table2.setContent(1, 0, "Now both elements in a nested row");
-	        table2.setContent(1, 1, getFlexBox(page, editNewProgramName, buttonSaveAsNewProgram, "twocolUpdate"));
+	        table2.setContent(1, 1, getHorizontalFlexBox(page, editNewProgramName, buttonSaveAsNewProgram, "twocolUpdate"));
 	}
 	
-	public static Flexbox getFlexBox(WidgetPage<?> page, OgemaWidget w1, OgemaWidget w2, String id) {
+	public static Flexbox getHorizontalFlexBox(WidgetPage<?> page, OgemaWidget w1, OgemaWidget w2, String id) {
 		Flexbox flex = new Flexbox(page, id, true);
+		flex.addItem(w1, null).addItem(w2, null);
+		flex.setJustifyContent(JustifyContent.SPACE_AROUND, null);
+		flex.setDefaultFlexWrap(FlexWrap.NOWRAP);
+		return flex;
+	}
+	
+	public static Flexbox getVerticalFlexBox(WidgetPage<?> page, OgemaWidget w1, OgemaWidget w2, String id) {
+		Flexbox flex = new Flexbox(page, id, true);
+		flex.setDefaultFlexDirection(FlexDirection.COLUMN);
 		flex.addItem(w1, null).addItem(w2, null);
 		flex.setJustifyContent(JustifyContent.SPACE_AROUND, null);
 		flex.setDefaultFlexWrap(FlexWrap.NOWRAP);
